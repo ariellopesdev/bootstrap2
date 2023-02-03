7e89a4980495763633c77f6938dda70c217c881e
ariellopesdev <lubyel.lopes@gmail.com>
2023-02-02 21:25:42 -0300

    applied responsive

--- js/scripts.js
+++ js/scripts.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
     setTimeout(function() {
         $('#data-area').parallax({imageSrc: 'imagem/cidadeparallax.png'});
         $('#apply-area').parallax({imageSrc: 'imagem/pattern.png'});
-    },250);
+    },200);
 
     /*Filtro do Portfolio*/
     $('.filter-btn').on('click', function(){
@@ -103,4 +103,37 @@ $(document).ready(function() {
             });
         }
     }
+    //Scroll para seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    letscrollTo = '';
+
+    $(navBtn).click(function () {
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu'){
+            scrollTo = aboutSection;
+        }else if(btnId == 'services-menu'){
+            scrollTo = servicesSection;
+        }else if(btnId == 'team-menu'){
+            scrollTo = teamSection;
+        }else if(btnId == 'portfolio-menu'){
+            scrollTo = portfolioSection;
+        }else if(btnId == 'contact-menu'){
+            scrollTo = contactSection;
+        }else{
+            scrollTo = bannerSection;
+        }
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        },1500);
+    })
+    
 });
